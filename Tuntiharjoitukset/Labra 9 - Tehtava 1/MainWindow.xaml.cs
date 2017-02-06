57a791d8f359781af4e194ef7902a21305d662ab
k8517 <k8517@student.jamk.fi>
2017-02-06 13:15:13 +0200

    Tehtävä 1 tehty

--- Tuntiharjoitukset/Labra 9 - Tehtava 1/MainWindow.xaml.cs
+++ Tuntiharjoitukset/Labra 9 - Tehtava 1/MainWindow.xaml.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/* **************************************
+Tuntitehtäviä.
+
+Luotu 6.2.2017
+
+Toiminta: Opiskelija on saanut itselleen unelma kesätyön vilkasliikenteisen kadun varrelta. 
+Hänen tulee laskea ohi menevät henkilö- ja kuorma-autot. Laadi opiskelijalle sovellus, jolla 
+hän voi laskea helposti ohi menneet autot. Painikkeita painettaessa ko. kulkuneuvon osoittama 
+lukumäärä kasvaa yhdellä.
+
+Minttu Mäkäläinen K8517 @ JAMK 
+************************************** */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +37,34 @@ namespace Labra_9___Tehtava_1
         {
             InitializeComponent();
         }
+
+        int i = 0, j = 0;
+        string teksti;
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            i++;
+            teksti = i.ToString();
+            textBlock.Text = teksti;
+            //MessageBox.Show("Hello " + textBox.Text, "VIRUS ATTACK! Copying files ...");
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            j++;
+            teksti = j.ToString();
+            textBlock_Copy.Text = teksti;
+            //MessageBox.Show("Hello " + textBox.Text, "VIRUS ATTACK! Copying files ...");
+        }
+        private void Grid_ContextMenuClosing(object sender, ContextMenuEventArgs e)
+        {
+
+        }
+
+    //    private void button_Copy_Click(object sender, RoutedEventArgs e)
+    //    {   // Kutsutaan näkyviin About -niminen ikkuna
+    //        About aboutWin = new About();           // Tehdään uusi olio
+    //        //aboutWin.ShowDialog();                  // Huom. tämä on modaalinen ikkuna = pääikkunalle ei voi tehdä mitään ennen kuin tämän sulkee
+    //        aboutWin.Show();                        // Ikkunoiden välillä voi siirtyä vapaasti sulkematta ensin
+    //    }
     }
 }
