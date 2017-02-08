d98d7a667454a7989bec6f0d417de962abbd2767
k8517 <k8517@student.jamk.fi>
2017-02-08 13:30:32 +0200

    Tuntiharjoitus

---
+++ Tuntiharjoitukset/Labra 10 - tuntiharjoitus/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿/* **************************************
+Tuntitehtäviä, WPF harjoittelua.
+
+Luotu 8.2.2017
+
+Toiminta: 
+
+Minttu Mäkäläinen K8517 @ JAMK 
+************************************** */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra_10___Tehtava_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            // ilmoitus tilariville
+            txbFooter.Text = "Painoit Exittiä, tätä ohjelmaa ei kuitenkaan voi lopettaa.";
+        }
+    }
+}
